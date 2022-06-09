0d116ff807142aab188f0b50bdbf8261414238ea
Yifan Wang <yifan.wang0422@gmail.com>
2022-06-08 20:12:55 -0400

    pushing what I have at end of lab session

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+
+import "./About.css";
+import YifanWang_Headshot from "../assets/YifanWang_Headshot.jpeg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={YifanWang_Headshot}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Yifan Wang</div>
+            <div className="brief_description">
+              Hello! My name is Yifan Wang, and I am a rising junior at MIT majoring in Computation and Cognition.
+              This summer I am participating in IBM Accelerate being part of the software track. 
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
